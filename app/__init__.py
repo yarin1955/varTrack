f8e0acab5876c45d9d16d5b2b52b303c2846a7b6
yarin <yarins1955@gmail.com>
2025-11-29 20:03:54 +0200

    bugfix double initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,13 @@
 from flask import Flask
 
 from .business_logic.initializer import initializer
-from .settings import load_config
 from .routers.webhooks import bp as webhooks_bp
 from .routers.tasks import bp as tasks_bp
 from .celery_app import init_celery
-import threading
 
 def create_app(config_data: dict | None = None) -> Flask:
     app = Flask(__name__)
 
-    if config_data is None:
-        config_data = load_config()
-
-    # Example: map your structured config into app.config
     app.config["SCHEMA_REGISTRY"] = config_data["schema_registry"]
 
     for platform in config_data.get("platforms", []):
@@ -34,16 +28,6 @@ def create_app(config_data: dict | None = None) -> Flask:
     app.register_blueprint(webhooks_bp, url_prefix="/webhooks")
     app.register_blueprint(tasks_bp, url_prefix="/tasks")
 
-
-    # Initialize Celery (attaches to app.extensions["celery"])
     init_celery(app)
-
-    # Start initializer in background thread (required in your case)
-    thread = threading.Thread(
-        target=initializer,
-        args=(config_data,),
-        daemon=True,
-    )
-    thread.start()
 
     return app
