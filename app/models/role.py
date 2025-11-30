70ff344d1086c727e5bc05dcdef228a70cdbec55
yarin <yarins1955@gmail.com>
2025-12-01 01:16:16 +0200

    refactor Role model structure

--- app/models/role.py
+++ app/models/role.py
@@ -1,17 +1,21 @@
 from pydantic import BaseModel, ConfigDict
-from typing import List, Union
+from typing import List, Union, Optional, Dict
 
 
 class Role(BaseModel):
-    """
-    Shared settings: endpoint + either a token or username/password.
-    """
     platform: str
     datasource: Union[str, List[str]]
-    fileName: str
-    envAsBranch: bool
-    prune: bool
+    fileName: Optional[str]
     repositories: List[str]
+    excludeRepositories: List[str]
+    envAsBranch: bool = False
+    envAsPR: bool = False
+    envAsTags: bool = False
+    branchMap: Optional[Dict[str, str]] = None
+    filePathMap: Optional[Dict[str, str]] = None
+    uniqueKeyName: str = "{repoName}-{env}"
+    variablesMap: Optional[Dict[str, str]] = None
+    #prune: bool = False
 
     model_config = ConfigDict(
         extra="ignore",         # ignore unknown fields
