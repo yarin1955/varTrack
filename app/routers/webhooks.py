18c8db407124f6fa1fbf168c4bdd8fcab05f718b
yarin <yarins1955@gmail.com>
2025-12-07 12:04:14 +0200

    move verifying signature from main agent to route

--- app/routers/webhooks.py
+++ app/routers/webhooks.py
@@ -3,7 +3,7 @@ from app.middlewares.webhooks import validate_route_param
 from app.utils.factories.datasource_factory import DataSourceFactory
 from app.utils.factories.platform_factory import PlatformFactory
 from ..tasks.main_agent import webhook_handler
-
+from ..utils.handlers.webhooks import WebhooksHandler
 
 bp = Blueprint('webhooks', __name__)
 
@@ -15,6 +15,20 @@ def handler_webhooks(platform, datasource):
     json_payload = request.get_json()
     raw_payload = request.data.decode('utf-8')
     headers=dict(request.headers)
+
+    platform_config = current_app.config.get(platform)
+    if not platform_config:
+        return jsonify({'status': 'error', 'message': 'Server configuration error'}), 500
+
+    try:
+        platform_instance = PlatformFactory.create(**platform_config)
+        signature = headers.get(platform_instance.git_scm_signature)
+
+        if not WebhooksHandler.verify_signature(platform_instance.secret, raw_payload, signature):
+            return jsonify({'status': 'unauthorized', 'message': 'Invalid signature'}), 401
+
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': 'Verification failed'}), 500
 
     task = webhook_handler.apply_async(
         args=[platform, datasource, raw_payload, json_payload, headers])
